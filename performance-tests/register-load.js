2b71a8307eb7bd6d3e456eb61eeb399096528dca
leramk <valeriavalerieva83@gmail.com>
2026-04-09 22:59:07 +0300

    Create register-load.js

---
+++ performance-tests/register-load.js
@@ -0,0 +1,8 @@
+import http from 'k6/http'; import { sleep } from 'k6';
+export const options = { stages: [
+{ duration: '5s', target: 5 },
+{ duration: '10s', target: 5 },
+{ duration: '5s', target: 0 },
+],
+};
+}
